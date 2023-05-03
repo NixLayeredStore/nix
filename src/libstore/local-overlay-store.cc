4aac582054203b763d155503f28d38e862761179
Ben Radford <benradf@users.noreply.github.com>
2023-05-03 13:46:17 +0100

    Delegate all LocalOverlayStore methods to LocalStore.

--- src/libstore/local-overlay-store.cc
+++ src/libstore/local-overlay-store.cc
@@ -29,7 +29,7 @@ LocalOverlayStore::LocalOverlayStore(const Params & params)
 LocalOverlayStore::LocalOverlayStore(std::string scheme, std::string path, const Params & params)
     : LocalOverlayStore(params)
 {
-    throw UnimplementedError("LocalOverlayStore");
+    //throw UnimplementedError("LocalOverlayStore");
 
     // TODO: Mount overlayfs
 }
@@ -51,14 +51,17 @@ std::string LocalOverlayStore::getUri()
 
 void LocalOverlayStore::registerDrvOutput(const Realisation & info)
 {
-    throw UnimplementedError("~LocalOverlayStore");
+    //throw UnimplementedError("~LocalOverlayStore");
 
     // TODO: queryRealisation(info.id)
+
+    return LocalStore::registerDrvOutput(info);
 }
 
 void LocalOverlayStore::queryPathInfoUncached(const StorePath & path,
     Callback<std::shared_ptr<const ValidPathInfo>> callback) noexcept
 {
+    /*
     try {
         // TODO: Try upper and then lower in turn.
         // If in lower but not upper, copy up.
@@ -66,6 +69,8 @@ void LocalOverlayStore::queryPathInfoUncached(const StorePath & path,
         throw UnimplementedError("queryPathInfoUncached");
 
     } catch (...) { callback.rethrow(); }
+    */
+    return LocalStore::queryPathInfoUncached(path, std::move(callback));
 }
 
 
@@ -74,7 +79,9 @@ bool LocalOverlayStore::isValidPathUncached(const StorePath & path)
     // TODO: Try upper and then lower in turn.
     // If in lower but not upper, copy up.
 
-    throw UnimplementedError("isValidPathUncached");
+    //throw UnimplementedError("isValidPathUncached");
+
+    return LocalStore::isValidPathUncached(path);
 }
 
 
@@ -82,7 +89,9 @@ StorePathSet LocalOverlayStore::queryAllValidPaths()
 {
     // TODO: Query both upper and lower and union the results.
     // Also insert the union into the upper so records can be referenced from foreign keys.
-    throw UnimplementedError("queryAllValidPaths");
+    //throw UnimplementedError("queryAllValidPaths");
+
+    return LocalStore::queryAllValidPaths();
 }
 
 
@@ -90,7 +99,9 @@ void LocalOverlayStore::queryReferrers(const StorePath & path, StorePathSet & re
 {
     // TODO: Query both upper and lower and union the results.
     // Also insert the union into the upper so records can be referenced from foreign keys.
-    throw UnimplementedError("queryReferrers");
+    //throw UnimplementedError("queryReferrers");
+
+    return LocalStore::queryReferrers(path, referrers);
 }
 
 
@@ -98,7 +109,9 @@ StorePathSet LocalOverlayStore::queryValidDerivers(const StorePath & path)
 {
     // TODO: Query both upper and lower and union the results.
     // Also insert the union into the upper so records can be referenced from foreign keys.
-    throw UnimplementedError("queryValidDerivers");
+    //throw UnimplementedError("queryValidDerivers");
+
+    return LocalStore::queryValidDerivers(path);
 }
 
 
@@ -107,25 +120,29 @@ LocalOverlayStore::queryPartialDerivationOutputMap(const StorePath & path_)
 {
     // TODO: Query both upper and lower and union the results.
     // Also insert the union into the upper so records can be referenced from foreign keys.
-    throw UnimplementedError("queryPartialDerivationOutputMap");
+    //throw UnimplementedError("queryPartialDerivationOutputMap");
+
+    return LocalStore::queryPartialDerivationOutputMap(path_);
 }
 
 std::optional<StorePath> LocalOverlayStore::queryPathFromHashPart(const std::string & hashPart)
 {
     // TODO: Something like queryPathInfoUncached (not quite since return type of this function is StorePath)
-    throw UnimplementedError("queryPathFromHashPart");
+    //throw UnimplementedError("queryPathFromHashPart");
+
+    return LocalStore::queryPathFromHashPart(hashPart);
 }
 
 
 void LocalOverlayStore::registerValidPaths(const ValidPathInfos & infos)
 {
-    throw UnimplementedError("registerValidPaths");
+    //throw UnimplementedError("registerValidPaths");
 
     // TODO: queryPathInfo(p) for each path p
     // This should ensure all paths are copied to upper db so they are available
     // for the internal query functions that registerValidPaths will call on the upper db.
 
-    LocalStore::registerValidPaths(infos);
+    return LocalStore::registerValidPaths(infos);
 }
 
 
@@ -134,44 +151,52 @@ void LocalOverlayStore::repairPath(const StorePath & path)
     // TODO: queryPathInfo(path)
     // To ensure paths are in upper db ...
 
-    LocalStore::repairPath(path);
+    return LocalStore::repairPath(path);
 }
 
 
 Roots LocalOverlayStore::findRoots(bool censor)
 {
-    throw UnimplementedError("findRoots");
+    //throw UnimplementedError("findRoots");
 
-    Roots roots;
+    //Roots roots;
 
-    // TODO: Warn that garbage collection is not supported.
+    //// TODO: Warn that garbage collection is not supported.
 
-    return roots;
+    //return roots;
+
+    return LocalStore::findRoots(censor);
 }
 
 
 void LocalOverlayStore::collectGarbage(const GCOptions & options, GCResults & results)
 {
-    throw UnimplementedError("collectGarbage");
+    //throw UnimplementedError("collectGarbage");
 
     // TODO: Warn that garbage collection is not supported.
+
+    return LocalStore::collectGarbage(options, results);
 }
 
 void LocalOverlayStore::optimiseStore()
 {
-    throw UnimplementedError("optimiseStore");
+    //throw UnimplementedError("optimiseStore");
 
     // TODO: Warn that store optimisation is not supported.
+
+    return LocalStore::optimiseStore();
 }
 
 
 bool LocalOverlayStore::verifyStore(bool checkContents, RepairFlag repair)
 {
-    throw UnimplementedError("verifyStore");
+    //throw UnimplementedError("verifyStore");
 
     // TODO: Warn that verifying the store is not supported.
 
-    return false;
+    //return false;
+
+    return LocalStore::verifyStore(checkContents, repair);
 }
 
 
@@ -180,7 +205,9 @@ void LocalOverlayStore::addSignatures(const StorePath & storePath, const StringS
     // TODO: queryPathInfo(storePath)
     // To ensure the paths are copied to the upper db.
 
-    throw UnimplementedError("addSignatures");
+    //throw UnimplementedError("addSignatures");
+
+    return LocalStore::addSignatures(storePath, sigs);
 }
 
 
@@ -189,13 +216,15 @@ void LocalOverlayStore::queryRealisationUncached(const DrvOutput & id,
 {
     // throw UnimplementedError("queryRealisationUncached");
 
-    try {
-        // TODO: Try upper and then lower in turn.
-        // If in lower but not upper, copy up.
+    //try {
+    //    // TODO: Try upper and then lower in turn.
+    //    // If in lower but not upper, copy up.
 
-    } catch (...) {
-        callback.rethrow();
-    }
+    //} catch (...) {
+    //    callback.rethrow();
+    //}
+
+    return LocalStore::queryRealisationUncached(id, std::move(callback));
 }
 
 
